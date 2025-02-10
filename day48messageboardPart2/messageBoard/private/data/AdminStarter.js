166af56c7aeb1c983037fddfb87efc2460cb411b
whoisatharv <atharvavyas72@gmail.com>
2025-02-10 23:58:51 +0530

    add todays work

---
+++ day48messageboardPart2/messageBoard/private/data/AdminStarter.js
@@ -0,0 +1,65 @@
+const fs = require("fs");
+var thisModule = this;
+exports.processRequest = function(request, response) {
+
+
+response.setContentType("text/html");
+response.write( "<!DOCTYPE HTML>");
+response.write("<HTML lang = 'eg'>");
+response.write("<head>");
+response.write( "<title> College Message board </title>");
+response.write( "<meta charset = 'utf-8'>");
+
+
+if (fs.existsSync("./private/data/admin.conf")) {
+    response.write( "<body>");
+    response.write( "<h1> Administrator Module </h1>");
+    response.write( "<h3> Authentication </h3>");
+    response.write( "<form action = 'authenticateAdmin' method = 'POST'>");
+    response.write( "<table border = '0' >");
+    response.write( "<tr>");
+    response.write( "<td> UserName </td>");
+    response.write( "<td><input type = 'text' id = 'username' name = 'username' maxlength = '15' size = '16' /> </td>");
+    response.write( "</tr>");
+    response.write( "<tr>");
+    response.write( "<td> Password</td>");
+    response.write( "<td> <input type = 'password' id = 'password' name = 'password' maxlength = '15' size = '16' /></td>");
+    response.write( "</tr>");
+    response.write( "<tr>");
+    response.write( "<td colspan = '2' align = 'center'>");
+    response.write( "<button type = 'submit' > Login </button>");
+    response.write( "</td>");
+    response.write( "</tr>");
+    response.write( "</table>");
+    response.write( "</form>");
+} else {
+
+    response.write( "<body>");
+    response.write( "<h1> Administrator Module </h1>");
+    response.write( "<h3> Setup Administrator Account </h3>");
+    response.write( "<form action = 'createAdmin' method = 'POST'>");
+    response.write( "<table border = '0' >");
+    response.write( "<tr>");
+    response.write( "<td> UserName </td>");
+    response.write( "<td><input type = 'text' id = 'username' name = 'username' maxlength = '15' size = '16' /> </td>");
+    response.write( "</tr>");
+    response.write( "<tr>");
+    response.write( "<td> Password</td>");
+    response.write( "<td> <input type = 'password' id = 'password' name = 'password' maxlength = '15' size = '16' /></td>");
+    response.write( "</tr>");
+    response.write( "<tr>");
+    response.write( "<tr>");
+    response.write( "<td> Renter Password</td>");
+    response.write( "<td> <input type = 'password' id = 'repassword' name = 'repassword' maxlength = '15' size = '16' /></td>");
+    response.write( "</tr>");
+    response.write( "<tr>");
+    response.write( "<td colspan = '2' align = 'center'>");
+    response.write( "<button type = 'submit' > Create </button>");
+    response.write( "</td>");
+    response.write( "</tr>");
+    response.write( "</table>");
+    response.write( "</form>");
+}
+  response.write( "</body>");
+  response.write( "</html>");
+}
