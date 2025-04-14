bff0d2bdd471c50d5c17692f3fbc36e4a67bb96b
whoisatharv <atharvavyas72@gmail.com>
2025-04-15 03:22:54 +0530

    todays work

---
+++ day80TMJrockpart10/learn/jst2js.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+
+exports.prepareJS = function (jstFileName, request) {
+ // if folder private/jst does not exist, create then
+	var privateFolder = "./private";
+	if (!fs.existsSync(privateFolder)) fs.mkdirSync(privateFolder);
+	var jstFolder = "./private/jst";
+
+	if(!fs.existsSync(jstFolder)) fs.mkdirSync(jstFolder);
+	// open file and create new filename with extension js in place of jst
+
+	var jsFileName = jstFileName.substring(0,jstFileName.length - 3);
+	jsFileName = jsFileName + "js";
+
+	var jsFilePath = "./private/jst/" +jsFileName;
+
+	var jsfileExist = fs.existsSync(jsFilePath);
+    var oldjstfile = false;
+    if (jsfileExist) {
+    // check if jst file is updated after creating js file so we need to re generate js file
+    	if (fs.statSync("StudentAddForm.jst").mtime > fs.statSync(jsFilePath).ctime)
+    	{
+    		oldjstfile = true;
+    	}
+    }
+	
+    if (!jsfileExist || oldjstfile) {
+    	var jsFile = fs.openSync(jsFilePath, "w");
+
+    	fs.writeSync(jsFile, "exports.processRequest = function (request, response)\r\n");
+    	fs.writeSync(jsFile,"{\r\n");
+    	var lines = fs.readFileSync(jstFileName).toString().split("\n");
+    	var line = null;
+    	for (i in lines) {
+    		line = lines[i].replace(/\r|\n/g,"");
+    		line = line.replace(/"/g, "\\\"");
+    		line = line.replace(/\$\$\$\{.*?\}/g, function (k) {
+
+    			k = k.substring(4, k.length -1);
+    			var v = request.data[k];
+    			console.log("Data:"+ v);
+
+    			if(v) return v; else return "";
+    		});
+    		fs.writeSync(jsFile, "response.write(\""+line+"\");\r\n");
+    	}
+    	fs.writeSync(jsFile, "response.close();\r\n");
+    	fs.writeSync(jsFile,"}\r\n");
+    	fs.closeSync(jsFile);
+    }
+		// return name of jsfile
+	return "jst/"+jsFileName;
+}
